4581f043caf7af34899b52c34cf9acc5622f4be1
Idzel Jaimes <irjaimes@outlook.com>
2020-12-20 09:23:28 -0800

    moved project files to new dir, created form in html, edited css styles

---
+++ Develop/script.js
@@ -0,0 +1,37 @@
+// Assignment code here
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+/* function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+} */
+
+
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "_", "~", "<", ">", "-", "+"]
+
+Math.floor(Math.random) 
